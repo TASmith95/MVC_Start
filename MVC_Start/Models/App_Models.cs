470ebd0f38d31b6f85c198fe74102a811acafbe0
TASmith95 <smith@LAPTOP-5USGC57A>
2021-06-25 03:09:44 -0400

    Second attempt to save changes for HTML and Razor form assignments.

--- MVC_Start/Models/App_Models.cs
+++ MVC_Start/Models/App_Models.cs
@@ -5,20 +5,27 @@ using System.Threading.Tasks;
 
 namespace MVC_Start.Models
 {
-  public class GuestContact
-  {
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-  }
 
-  public class ChartModel
-  {
-    public string ChartType { get; set; }
-    public string Labels { get; set; }
-    public string Data { get; set; }
-    public string Title { get; set; }
-  }
+    public class FavoriteColor
+    {
+        public string FirstColor { get; set; }
+        public string SecondColor { get; set; }
+        public string ThirdColor { get; set; }
+    }
+    public class GuestContact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
 
-  
+    public class ChartModel
+    {
+        public string ChartType { get; set; }
+        public string Labels { get; set; }
+        public string Data { get; set; }
+        public string Title { get; set; }
+    }
+
+
 }
